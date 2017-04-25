aea4447ec6a158d21c2cf737c87418ffda2bc070
IMUDGES-YY <929144090@qq.com>
2017-04-25 21:01:45 +0800

    update

--- app/src/main/java/com/imudges/mytask/mytask/MainActivity.java
+++ app/src/main/java/com/imudges/mytask/mytask/MainActivity.java
@@ -1,15 +1,29 @@
 package com.imudges.mytask.mytask;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import es.dmoral.toasty.Toasty;
+import org.xutils.view.annotation.ContentView;
+import org.xutils.view.annotation.Event;
+import org.xutils.view.annotation.ViewInject;
 
-public class MainActivity extends AppCompatActivity {
+@ContentView(R.layout.activity_main)
+public class MainActivity extends BaseActivity {
 
+
+    @ViewInject(R.id.btn_test)
+    private Button btnTest;
+
+    @Event(value = R.id.btn_test,type = View.OnClickListener.class)
+    public void testButtonClick(View v) { // 方法签名必须和接口中的要求一致
+        Toasty.success(MainActivity.this,"hello world",Toast.LENGTH_SHORT,true).show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
     }
 }
