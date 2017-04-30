aca92c4690ae8d1736b3d3b5880d6cdb86c0aad0
IMUDGES-YY <929144090@qq.com>
2017-04-30 13:23:33 +0800

    完成MyAdapter，以及搜索用的Filter

--- app/src/main/java/com/imudges/mytask/Adapter/MyAdapter.java
+++ app/src/main/java/com/imudges/mytask/Adapter/MyAdapter.java
@@ -1,6 +1,7 @@
 package com.imudges.mytask.Adapter;
 
 import android.content.Context;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,23 +10,29 @@ import com.imudges.mytask.Listener.MyClickListener;
 import com.imudges.mytask.R;
 import org.w3c.dom.Text;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 /**
  * Created by yangyang on 2017/4/29.
  */
-public class MyAdapter extends BaseAdapter {
+public class MyAdapter extends BaseAdapter implements Filterable{
 
     private List<Map<String,String>> mContentList;
+    private List<Map<String,String>> mBackContentList;//作为备用
     private MyClickListener myClickListener;
     private LayoutInflater mInflater;
+    MyFilter mFilter = null;
 
     public MyAdapter(Context context, List<Map<String, String>> mContentList, MyClickListener myClickListener) {
         this.mContentList = mContentList;
         this.myClickListener = myClickListener;
         mInflater = LayoutInflater.from(context);
+        mBackContentList = mContentList;
     }
+
+
 
     @Override
     public int getCount() {
@@ -106,6 +113,14 @@ public class MyAdapter extends BaseAdapter {
         return convertView;
     }
 
+    @Override
+    public Filter getFilter() {
+        if (mFilter == null) {
+            mFilter = new MyFilter();
+        }
+        return mFilter;
+    }
+
     public class ViewHolder{
         public ImageView imgTaskType;//任务类型
         public TextView tvTaskName;
@@ -118,5 +133,47 @@ public class MyAdapter extends BaseAdapter {
         public Button btnCommit;//提交
         public Button btnEdit;//编辑
         public Button btnAbandon;//放弃
+    }
+
+    //搜索用
+    //我们需要定义一个过滤器的类来定义过滤规则
+    class MyFilter extends Filter{
+
+        //我们在performFiltering(CharSequence charSequence)这个方法中定义过滤规则
+        @Override
+        protected FilterResults performFiltering(CharSequence constraint) {
+            FilterResults results = new FilterResults();
+            List<Map<String,String>> list;
+            //当过滤的关键字为空时，我们显示所有的数据
+            if(TextUtils.isDigitsOnly(constraint)){
+                list = mContentList;
+            } else {
+                list = new ArrayList<>();
+                for(Map<String,String> map :mBackContentList){
+                    //添加符合过滤原则的数据
+                    if(map.get("tv_task_name").contains(constraint) ||
+                            map.get("tv_suumary").contains(constraint) ||
+                            map.get("tv_add_time").contains(constraint) ||
+                            map.get("tv_task_status").contains(constraint)){
+                        list.add(map);
+                    }
+
+                }
+            }
+            results.values = list;//将得到的数据集放到FilterResult的value中
+            results.count = list.size();//将得到的数据集的大小放到FilterResult的count中
+            return results;
+        }
+
+        //在publishResults方法中告诉适配器更新界面
+        @Override
+        protected void publishResults(CharSequence constraint, FilterResults results) {
+            mContentList = (List<Map<String, String>>) results.values;
+            if(results.count > 0){
+                notifyDataSetChanged();//通知数据发生了变化
+            } else {
+                notifyDataSetInvalidated();//数据加载失败
+            }
+        }
     }
 }
